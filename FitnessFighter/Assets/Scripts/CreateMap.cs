eac9b955754888c97f48d18fea8cc97ed83a671a
gomholt <gomholt113@gmail.com>
2020-07-27 17:46:35 -0700

    open door to wall fix

--- FitnessFighter/Assets/Scripts/CreateMap.cs
+++ FitnessFighter/Assets/Scripts/CreateMap.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.Design.Serialization;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 //using rooms = GameManager.SharedInstance.rooms;
 
@@ -9,14 +10,34 @@ public class CreateMap : MonoBehaviour
 {
     private Dictionary<Vector2, GameObject> map;
     private GameManager gameManager;
-    //private RoomsCollections rooms = GameManager.SharedInstance.rooms;
     private Queue<GameObject> bfsQueue;
     private GameObject startRoom;
     private GameObject endRoom;
     private int size = 0;
+    private Dictionary<Vector4, GameObject> doorsToRoomMap;
+
+    private void Awake()
+    {
+        foreach(GameObject room in GameManager.SharedInstance.rooms.rooms)
+        {
+            doorsToRoomMap.Add(room.GetComponent<RoomType>().doors, room);
+        }
+    }
 
     void Start()
     {
+
+
+        while (!bfsMapCreation()) ;
+        //TODO: if queue still has stuff after reaching max size remove doors by checking the area around
+        //      open doors if one side is open and not the other.
+    }
+
+
+    private bool bfsMapCreation()
+    {
+        bool validMap = true;
+        
         map = new Dictionary<Vector2, GameObject>();
         bfsQueue = new Queue<GameObject>();
         RoomsCollections rooms = GameManager.SharedInstance.rooms;
@@ -32,7 +53,6 @@ public class CreateMap : MonoBehaviour
             if (!map.ContainsKey(currRoom.GetComponent<RoomType>().coord))
             {
                 map.Add(currRoom.GetComponent<RoomType>().coord, currRoom);
-                
             }
             else
             {
@@ -41,51 +61,99 @@ public class CreateMap : MonoBehaviour
 
             if (currRoom.GetComponent<RoomType>().Top)
             {
-                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + new Vector2(0, 1);
+                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + Vector2.up;
                 if (!map.ContainsKey(tempCoord))
                 {
                     GameObject tempRoom = rooms.bottomRooms[Random.Range(0, rooms.bottomRooms.Count)];
                     tempRoom.GetComponent<RoomType>().coord = tempCoord;
                     bfsQueue.Enqueue(tempRoom);
                 }
+                else
+                {
+                    GameObject adjRoom = map[tempCoord];
+                    RoomType adjRoomType = adjRoom.GetComponent<RoomType>();
+
+                    if (!adjRoomType.Bottom)
+                    {
+                        adjRoom = doorsToRoomMap[adjRoomType.doors + new Vector4(0, 0, 1, 0)];
+                        adjRoom.GetComponent<RoomType>().coord = adjRoomType.coord;
+                    }
+                }
             }
 
             if (currRoom.GetComponent<RoomType>().Right)
             {
-                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + new Vector2(1, 0);
+                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + Vector2.right;
                 if (!map.ContainsKey(tempCoord))
                 {
                     GameObject tempRoom = rooms.leftRooms[Random.Range(0, rooms.leftRooms.Count)];
                     tempRoom.GetComponent<RoomType>().coord = tempCoord;
                     bfsQueue.Enqueue(tempRoom);
                 }
+                else
+                {
+                    GameObject adjRoom = map[tempCoord];
+                    RoomType adjRoomType = adjRoom.GetComponent<RoomType>();
+
+                    if (!adjRoomType.Left)
+                    {
+                        adjRoom = doorsToRoomMap[adjRoomType.doors + new Vector4(0, 0, 0, 1)];
+                        adjRoom.GetComponent<RoomType>().coord = adjRoomType.coord;
+                    }
+                }
             }
 
             if (currRoom.GetComponent<RoomType>().Bottom)
             {
-                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + new Vector2(0, -1);
+                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + Vector2.down;
                 if (!map.ContainsKey(tempCoord))
                 {
                     GameObject tempRoom = rooms.topRooms[Random.Range(0, rooms.topRooms.Count)];
                     tempRoom.GetComponent<RoomType>().coord = tempCoord;
                     bfsQueue.Enqueue(tempRoom);
                 }
+                else
+                {
+                    GameObject adjRoom = map[tempCoord];
+                    RoomType adjRoomType = adjRoom.GetComponent<RoomType>();
+
+                    if (!adjRoomType.Top)
+                    {
+                        adjRoom = doorsToRoomMap[adjRoomType.doors + new Vector4(1, 0, 0, 0)];
+                        adjRoom.GetComponent<RoomType>().coord = adjRoomType.coord;
+                    }
+                }
             }
 
             if (currRoom.GetComponent<RoomType>().Left)
             {
-                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + new Vector2(-1, 0);
+                Vector2 tempCoord = currRoom.GetComponent<RoomType>().coord + Vector2.left;
                 if (!map.ContainsKey(tempCoord))
                 {
                     GameObject tempRoom = rooms.rightRooms[Random.Range(0, rooms.rightRooms.Count)];
                     tempRoom.GetComponent<RoomType>().coord = tempCoord;
                     bfsQueue.Enqueue(tempRoom);
                 }
+                else
+                {
+                    GameObject adjRoom = map[tempCoord];
+                    RoomType adjRoomType = adjRoom.GetComponent<RoomType>();
+
+                    if (!adjRoomType.Right)
+                    {
+                        adjRoom = doorsToRoomMap[adjRoomType.doors + new Vector4(0, 1, 0, 0)];
+                        adjRoom.GetComponent<RoomType>().coord = adjRoomType.coord;
+                    }
+                }
             }
         }
 
-        //TODO: if queue still has stuff after reaching max size remove doors by checking the area around
-        //      open doors if one side is open and not the other.
+        if(size != 15)
+        {
+            validMap = false;
+        }
+
+        return validMap;
     }
 
 
