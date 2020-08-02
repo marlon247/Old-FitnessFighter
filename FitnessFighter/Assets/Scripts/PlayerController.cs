32046fb03a8b2e5fb7f59ee2213376a999b29ad2
Edwin Tang <31362346+Tang00@users.noreply.github.com>
2020-08-01 18:19:12 -0700

    added player movement (#5)

--- FitnessFighter/Assets/Scripts/PlayerController.cs
+++ FitnessFighter/Assets/Scripts/PlayerController.cs
@@ -4,13 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Update is called once per frame
-    void Update()
-    {
 
-        Vector3 mouse = Input.mousePosition;
-        Vector3 mouseWorld = Camera.main.ScreenToWorldPoint(new Vector3( mouse.x, mouse.y, transform.position.y));
-        Vector3 forward = mouseWorld - transform.position;
-        transform.rotation = Quaternion.LookRotation(forward, Vector3.up);
+    public float speed;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    //Update is called once per frame
+    void FixedUpdate()
+    {
+        //Player moves with WASD inputs
+        float Horizontal = Input.GetAxis("Horizontal");
+        float Vertical = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(Horizontal, 0.0f, Vertical) * speed * Time.deltaTime;
+        transform.position += move;
+
+        //Player faces mouse position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, -angle, 0));
     }
 }
